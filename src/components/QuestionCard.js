59e2b9b199c9b39564ff68b0bbf04cbddc86570f
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-02-10 12:23:07 +0530

    k

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../stylesheet/Question.scss"
+export const QuestionCard = () => {
+  return (
+    
+      <div className='card'>
+        
+      <p>1.Anthor name for vimal Kumar</p>
+      <button>Option 1</button>
+      <button>Option 2</button>
+      <button>Option 3</button>
+      <button>Option 4</button>
+      </div>
+    
+  )
+}
