167cdf736bf2755898e108a528f62ecf4d88962f
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-02-13 11:35:40 +0530

    13-feb

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import React from 'react'
 
 import DashBoard from './DashBoard';
 import Question from './components/Question.js';
-
+import Profile from "./components/Profile.js";
+import Menu from './components/Menu.js';
 
 function App() {
-  return (
-      <Question/>
+  return (<>
+      <Menu />
+      <Profile /></>
   );
 }
 
