ac2ff8b971a046f58ab59fd6ea545369f37bd315
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-02-14 13:57:23 +0530

    n

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-const Timer = () => {
-  const [time, setTime] = useState(3600);
+function Timer() {
+  const [time, setTime] = useState(3600); // initial time in seconds
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(time - 1);
+    const timer = setTimeout(() => {
+      if (time > 0) {
+        setTime(time - 1);
+      }
     }, 1000);
-
-    return () => clearInterval(intervalId);
+    return () => clearTimeout(timer);
   }, [time]);
-  var i=Math.round(time/60);
 
-  return (<div>{i}:{time-(i*60)} seconds left</div>);
-};
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
 
-export default Timer;
+  return (
+    <div>   
+     Remaining Time: {minutes}:{seconds < 10 ? "0" : ""}{seconds}
+    </div>
+  );
+}
+
+export default Timer
